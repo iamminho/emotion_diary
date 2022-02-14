6c6c03181ea345e403690afe89508e0c66acc18e
softpaper95 <minho100227@gamil.com>
2022-02-14 11:24:44 +0900

    select function

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -4,6 +4,7 @@ const DiaryEditor = () => {
     const [state, setState] = useState({
         author: "",
         content: "",
+        emotion: 1,
     });
 
     const handleChangeState = (e) => {
@@ -33,6 +34,19 @@ const DiaryEditor = () => {
                         });                        
                     }}                    
                 />
+            </div>
+            <div>
+                <select
+                    name='emotion'
+                    value={state.emotion}
+                    onChange = {handleChangeState}
+                >
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                    <option value={4}>4</option>
+                    <option value={5}>5</option>                    
+                </select>
             </div>
         </div>
     );
