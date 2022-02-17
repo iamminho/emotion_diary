a1aad9001ff77ed4796993a8ac1b400f6c30e0c0
softpaper95 <minho100227@gamil.com>
2022-02-17 19:15:21 +0900

    bring DiaryItem to DiaryList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import './App.css';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+
+const dummyList = [
+  {
+    id: 1,
+    author: "이름",
+    content: "hellow?",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "이름2",
+    content: "hellow!",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "이름3",
+    content: "hellow+",
+    emotion: 4,
+    created_date: new Date().getTime(),
+  },
+  
+];
 
 function App() {
   return (
     <div>      
-      <DiaryEditor/>
+      <DiaryEditor />
+      <DiaryList diaryList = {dummyList} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/DiaryList.js
@@ -0,0 +1,26 @@
+import DiaryItem from './DiaryItem';
+const DiaryList = ({ diaryList }) => {
+    return (
+        <div className="DiaryList">
+            <h2>일기 리스트</h2>
+            <h4>{diaryList.length}개의 일기가 있습니다.</h4>
+            <div>
+                {diaryList.map((it) => (
+                    <DiaryItem key = {it.id} {...it} />                    
+                ))}
+            </div>
+        </div> 
+    );   
+}
+
+DiaryList.defaultProps = {
+    diaryList: [],
+};
+
+export default DiaryList;
+/* <div key = {it.id}>
+                        <div>작성자: {it.author}</div>
+                        <div>내용: {it.content}</div>
+                        <div>감정지수: {it.emotion}</div>
+                        <div>날짜: {it.created_date}</div>
+                    </div> */
