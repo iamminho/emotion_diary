379f4e81cd84ba0db07471d842c8a61f6c4b51fb
softpaper95 <minho100227@gamil.com>
2022-02-21 21:58:19 +0900

    delete data

--- src/App.js
+++ src/App.js
@@ -46,10 +46,19 @@ function App() {
     dataId.current += 1;
     setData([newItem, ...data]); //...data는 원래데이터 + newItem은 새로운 데이터
   }
+
+  const onDelete = (targetId) => {
+    console.log(`${targetId}가 삭제되었습니다.`)
+    const newDiaryList = data.filter((it) => it.id !== targetId);
+    console.log(newDiaryList);
+    setData(newDiaryList);
+    alert(`삭제되었습니다.`);
+  };
+
   return (
     <div>      
       <DiaryEditor onCreate = {onCreate} />
-      <DiaryList diaryList = {data} />
+      <DiaryList onDelete ={onDelete} diaryList = {data} />
     </div>
   );
 };
